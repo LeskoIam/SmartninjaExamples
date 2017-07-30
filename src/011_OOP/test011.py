ab4cfe170d5b098b0c2e42c4f17f5f79206a68e3
Lesko <polensek.matevz@gmail.com>
2017-07-30 14:54:29 +0200

    Minor fixes.

--- src/011_OOP/test011.py
+++ src/011_OOP/test011.py
@@ -1,4 +1,6 @@
 __author__ = 'mpolensek'
+
+
 # Documentation is like sex.
 # When it's good, it's very good.
 # When it's bad, it's better than nothing.
@@ -8,29 +10,28 @@ __author__ = 'mpolensek'
 # ############ Classi in objekti ##############
 # Sintakticna definicija. Vsak class, po dogovoru, podeduje od osnovnega Python objekta 'object'
 class Zgradba(object):
-
     # Posebna metoda __init__ se izvede ko iz classa ustvarimo objekt (pri klicu)
     # http://stackoverflow.com/a/8609238
     def __init__(self, ime, st_oken, st_vrat):
-        self.ime = ime          # Podatne, zunanj, argumente preipisemo notranjim spremenljivkam
+        self.ime = ime  # Podatne, zunanj, argumente preipisemo notranjim spremenljivkam
         self.st_oken = st_oken  # Self predstavlja class sam
         self.st_vrat = st_vrat  # in se uporablja za sklicovanje na spremenljivke in metode v samem class-u
         # e.g.:
-        self.pokazi_st_vrat()   # Poklicemo class metodo
+        self.pokazi_st_vrat()  # Poklicemo class metodo
 
-    def pokazi_st_vrat(self):   # Metoda
+    def pokazi_st_vrat(self):  # Metoda
         print "Zgradba {} ima vrat.".format(self.ime, self.st_vrat)
+
 
 # Iz class-a ustvarimo objekt
 zg = Zgradba("baraka", 1, 1)  # V "tej vrstici" se izvede __init__ metoda
 zg.pokazi_st_vrat()  # Klic metode
-print zg.st_oken     # Poglejmo se argument
+print zg.st_oken  # Poglejmo se argument
 
 
 # Dedovanje objektov
 # Namesto da dedujemo od osnovnega objekta, lahko dedujemo od katerega koli drugegea objekta
 class Hisa(Zgradba):
-
     def __init__(self, ime, st_oken, st_vrat):
         # Pri dedovanju se dedovana __init__ metoda ne izvede, zato moramo za to poskrbeti sami
         # in podati dobljene argumente
@@ -38,6 +39,7 @@ class Hisa(Zgradba):
 
     def pokazi_st_oken(self):  # Metoda
         print "Hisa '{}' ima {} vrat".format(self.ime, self.st_oken)
+
 
 # Iz class-a ustvarimo objekt
 hi = Hisa("topli dom", 6, 2)  # V "tej vrstici" se izvede __init__ metoda
@@ -55,11 +57,11 @@ class Blok(Hisa):
     def pokazi_st_dvigal(self):  # Metoda
         print "Blok '{}' ima {} streh".format(self.ime, self.st_dvigal)
 
+
 bl = Blok("zelena jama", 160, 70, 2)
 bl.pokazi_st_dvigal()
 bl.pokazi_st_vrat()  # Metoda podedovana iz class-a Zgradba
 bl.pokazi_st_oken()  # Metoda podedovana iz class-a Hisa
-
 
 # ############ Primeri uporabe objektov ##############
 
@@ -82,6 +84,7 @@ for vsaka_hisa in hise:
 class PrenosPodatkov:
     pass
 
+
 # Nato v objekt lahko poimensko shranimo podatke
 a = PrenosPodatkov()
 a.rezultat_1 = 5
@@ -97,5 +100,7 @@ print a.rezultat_1, a.error_code, a.unit
 class A(object):
     def __init__(self):
         self.a = 1
+
+
 a = A()
 print a.a
